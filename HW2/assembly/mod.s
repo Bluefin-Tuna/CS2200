791df925480fc6faf8a7145d8d656fc47f9053b4
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-09-14 16:43:46 -0400

    updates

--- HW2/assembly/mod.s
+++ HW2/assembly/mod.s
@@ -8,3 +8,12 @@
 mod:
     addi    $a0, $zero, 28      ! $a0 = 28, the number a to compute mod(a,b)
     addi    $a1, $zero, 13      ! $a1 = 13, the number b to compute mod(a,b)
+    nand    $t0, $a1, $a1       ! $t0 = ~$a1, ~b
+    addi    $t0, $t0, 1         ! $t0 = -$a1, -b
+loop:
+    blt     $a0, $a1, done      ! if x < b go to done
+    add     $a0, $a0, $t0       ! x = x - b
+    beq     $zero, $zero, loop
+done:
+    add     $v0, $zero, $a0     ! return x
+    halt
